e4c837e389a024d92afaf39bd1f15c8e60feab31
xionghao <2576181740@qq.com>
2020-09-24 14:45:31 +0800

    复制配置bug

--- src/views/domain/copy_configuration.vue
+++ src/views/domain/copy_configuration.vue
@@ -432,7 +432,7 @@ export default {
 			this.urldisable.forEach((item, index) => {
 				this.copydatalist.base_config.url_name = item.url;
 				let jsonobj = new Object();
-				jsonobj = Object.assign({}, this.configuration);
+				jsonobj = Object.assign({}, this.configuration.cache_config);
 				// jsonobj.base_config = new Object();
 				// jsonobj.base_config = Object.assign(
 				// 	{},
